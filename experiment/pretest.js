df2d22fe20014146af1c21196a7d200e9fcb9d25
sravanthimodepu <sravanthimodepu@gmail.com>
2021-03-02 15:56:04 +0530

    added content

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -82,32 +82,121 @@
 
 
 
-
-
-
      /////////////// Write the MCQ below in the exactly same described format ///////////////
 
 
      const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+            question: "1. Different conformations of the protein differ only in:", ///// Write the question inside double quotes
             answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                a: "distance between the bonds of backbone and amino acid side chains", ///// Write the option 1 inside double quotes
+                b: "number of amino acid side-chains", ///// Write the option 2 inside double quotes
+		c: "angle of rotation about the bonds of backbone and amino acid side-chains", ///// Write the option 1 inside double quotes
+                d: "measure of dihedral angle at the peptide", ///// Write the option 2 inside double quotes
             },
             correctAnswer: "a" ///// Write the correct option inside double quotes
         },
 
     {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
+      question: "2. Chemical denaturation denotes marked change in protein structure of the native protein in response to: ",  ///// Write the question inside double quotes
       answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
+        a: "change in temperature",                  ///// Write the option 1 inside double quotes
+        b: "acid or alkali",                  ///// Write the option 2 inside double quotes
+        c: "Disruption of weak interactions by boiling",
+	d: "Exposure to detergents", 
+      },
       correctAnswer: "c"                ///// Write the correct option inside double quotes
     },
 
 
+      {
+      question: "3.  Which of the following chemical denaturant is not an inorganic salt? ",  ///// Write the question inside double quotes
+      answers: {
+        a: "Lithium bromide",                  ///// Write the option 1 inside double quotes
+        b: "Potassium thiocyanate",                  ///// Write the option 2 inside double quotes
+          c: "Dimethyl Formamide",
+	  d: "Sodium iodide",
+      },
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
+    },
 
+
+      {
+      question: "4. What mol/l of Urea acts as a Chaotrophic chemical denaturant?  ",  ///// Write the question inside double quotes
+      answers: {
+        a: "7-14 mol/l",                  ///// Write the option 1 inside double quotes
+        b: "6-8 mol/l",                  ///// Write the option 2 inside double quotes
+        c: "10 -12 mol/l",
+	d: "2-4 mol/l",
+      },
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
+    },
+ 
+
+      {
+      question: "5.  Acids and bases denature a protein by disrupting: ",  ///// Write the question inside double quotes
+      answers: {
+        a: "peptide bonds and ionic bonds",                  ///// Write the option 1 inside double quotes
+        b: "hydrophobic interactions and peptide bonds",                  ///// Write the option 2 inside double quotes
+        c: "ionic bonds and hydrogen bonds",
+	d: "amide bonds and alkene bonds"
+	e: "ionic bonds and hydrophobic interactions"
+      },
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
+    },
+
+
+      {
+      question: "6. What kind of interactions are not part of the protein tertiary structure ?  ",  ///// Write the question inside double quotes
+      answers: {
+        a: "disulphide bonds",                  ///// Write the option 1 inside double quotes
+        b: "hydrophobic interactions",                  ///// Write the option 2 inside double quotes
+        c: "salt bridges",
+	d: "hydrophilic interactions",
+	e: "peptide bonds",	  
+	  
+      },
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
+    },
+
+
+      {
+	  question: "7. The most important contribution to the stability of a protein's conformation appears to be the:  ",  ///// Write the question inside double quotes
+      answers: {
+        a: "entropy increase from the decrease in ordered water molecules forming a solvent shell around it.",                  ///// Write the option 1 inside double quotes
+        b: "maximum entropy increase from ionic interactions between the ionized amino acids in a protein.",                  ///// Write the option 2 inside double quotes
+        c: "sum of free energies of formation of many weak interactions among the hundreds of amino acids in a protein",
+
+	d: "sum of free energies of formation of many weak interactions between its polar amino acids and surrounding water.",
+	  
+      },
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
+    },
+
+
+      {
+      question: "8. Which of the following chemical denaturant is a cross-linking reagent?  ",  ///// Write the question inside double quotes
+      answers: {
+        a: "Formaldehyde",                  ///// Write the option 1 inside double quotes
+        b: "Dimethyl Formamide.",                  ///// Write the option 2 inside double quotes
+        c: "Formamide.",
+	d: "Sodium dodecyl sulphate.",
+      },
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
+    },
+
+
+       {
+      question: "9. An average protein will not be denatured by:  ",  ///// Write the question inside double quotes
+      answers: {
+        a: "a detergent such as sodium dodecyl sulfate.",                  ///// Write the option 1 inside double quotes
+        b: "iodoacetic acid.",                  ///// Write the option 2 inside double quotes
+        c: "heating to 90°C.",
+	d: "urea.",
+      },
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
+    },
+
+			  
      ];
 
 
